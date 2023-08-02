22d8c0e0b82f42fbf5f632d5d90fe3b101eeaa30
AlexOz-UA <alexclans2015@gmail.com.com>
2023-08-02 21:22:04 +0200

    animation for burger menu

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -31,23 +31,16 @@ const Navbar = () => {
     <nav className="navbar">
       <h1>React Blog</h1>
       <div className="links">
-        {/* {isRegistered && <Link to="/create">New Blog</Link>}
-        {isRegistered && <Link to="/blogs-liked">Saved Blogs</Link>}
-        {!isRegistered && <Link to="/register">Register</Link>}
-        {!isRegistered && <Link to="/login">Login</Link>}
-        {handleAdminCheck() && <Link to="/adminpanel">Admin</Link>}
-        {isRegistered && (
-          <button style={{ marginLeft: "23px" }} onClick={handleLogoutAction}>
-            Logout
-          </button>
-        )} */}
         <nav>
-          <div className="burger-menu-icon" onClick={handleBurgerClick}>
+          <div
+            className={`burger-menu-icon ${menuOpen ? "open" : ""}`}
+            onClick={handleBurgerClick}
+          >
             <div className={`line ${menuOpen ? "open" : ""}`} />
             <div className={`line ${menuOpen ? "open" : ""}`} />
             <div className={`line ${menuOpen ? "open" : ""}`} />
           </div>
-          <ul className={`menu-list ${menuOpen ? "open" : ""}`}>
+          <ul className={`menu-list ${menuOpen ? "open" : "closed"}`}>
             <li className="burger-link">
               <Link to="/">Home</Link>
             </li>
@@ -87,14 +80,17 @@ const Navbar = () => {
               </li>
             )}
             <div className="burger-buttons">
-            {isRegistered && (
-              <li>
-                <button onClick={handleLogoutAction}>Logout</button>
-              </li>
-            )}
-            {menuOpen && <li>
-              <button onClick={() => setMenuOpen(false)}>Close</button>
-            </li>}
+              {isRegistered && (
+                <li
+                  style={{
+                    marginTop: "15px",
+                    right: "-5px",
+                    position: "relative",
+                  }}
+                >
+                  <button onClick={handleLogoutAction}>Logout</button>
+                </li>
+              )}
             </div>
           </ul>
         </nav>
